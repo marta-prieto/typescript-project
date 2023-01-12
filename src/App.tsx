adf732d826547824670a666099e45cc287ca84cf
marta.prieto <marta.prieto@ac247202r.onmicrosoft.com>
2023-01-12 21:06:22 +0100

    Add first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const INITIAL_STATE = [
+  {
+    nick: 'even',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/300',
+    description: 'moderador'
+  },
+  {
+    nick: 'violeta',
+    subMonths: 6,
+    avatar: 'https://i.pravatar.cc/400',
+
+  }
+]
+
+interface Sub {
+  nick: string
+  avatar: string
+  subMonths: number
+  description?: string
+}
+
+
 function App() {
+  const [subs, setSubs] = useState<Array<Sub>>([])
+
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE)
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Subscriptors</h1>
+      <ul>
+        {
+          subs.map(sub => {
+            return (
+              <li key={sub.nick}>
+                <img src={sub.avatar} alt={`Avatar for $(sub.nick)`} />
+                <h4>{sub.nick}(<small>{sub.subMonths}</small>)</h4>
+                <p>{sub.description?.substring(0, 100)}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
