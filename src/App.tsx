44fb5be59a48690a80014dd4e1cf0f2182809089
marta.prieto <marta.prieto@ac247202r.onmicrosoft.com>
2023-05-02 12:59:30 +0200

    Add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState , useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import List from './components/List';
 import Form from './components/Form';
-import {Sub} from './types'
+import { Sub, SubsResponseFromApi } from './types';
+//import { subs } from '../subs.json';
 
 
 interface AppState {
@@ -11,40 +12,55 @@ interface AppState {
 }
 
 
-const INITIAL_STATE = [
-  {
-    nick: 'even',
-    subMonths: 3,
-    avatar: 'https://i.pravatar.cc/300',
-    description: 'moderador'
-  },
-  {
-    nick: 'violeta',
-    subMonths: 6,
-    avatar: 'https://i.pravatar.cc/400',
 
-  }
-]
 
 function App() {
   const [subs, setSubs] = useState<AppState["subs"]>([])
   const [newSubsNumber, setNewSubsNumber] = useState<AppState[
-    "newSubsNumber"]>(0) 
-    const divRef = useRef<HTMLDivElement>(null)
+    "newSubsNumber"]>(0)
+  const divRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    setSubs(INITIAL_STATE)
+    const fetchSubs = (): Promise<SubsResponseFromApi> => {
+      return fetch('http://localhost:3000/subs').then(res => res.json())
+    }
+
+    const mapFromApiToSubs = (apiResponse: SubsResponseFromApi): 
+    Array<Sub> => {
+      return apiResponse.map(subFromApi => {
+        const {
+          months: subMonths,
+          profileUrl: avatar,
+          nick,
+          description
+        } = subFromApi
+        
+        return {
+          nick,
+          description,
+          avatar,
+          subMonths
+        }
+      })
+    }
+
+    fetchSubs()
+      .then(mapFromApiToSubs)
+      .then(setSubs) 
+     // .catch(err => console.log('Solicitud fallida', err))
   }, [])
 
-const handleNewSub = (newSub: Sub) : void => {
-  setSubs(subs => [...subs, newSub])
-}
+  const handleNewSub = (newSub: Sub): void => {
+    setSubs(subs => [...subs, newSub])
+    setNewSubsNumber(n => n + 1)
+  }
 
   return (
     <div className="App" ref={divRef}>
       <h1>Subscriptors</h1>
       <List subs={subs} />
-      <Form onNewSub={handleNewSub}/>
+      New subs: {newSubsNumber}
+      <Form onNewSub={handleNewSub} />
     </div>
   );
 }
